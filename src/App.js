2dc00153a262614da7cb1de76e684dd566be55cd
Adham <adhamezaldin@gmail.com>
2022-11-07 00:35:07 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+
+import {Header,Container,Footer,components} from './components/index'
+
+const App = () => {
+    return (
+        <>
+        <Header />
+        <Container />
+        <Footer />
+        </>
+    );
+}
+
+export default App;
---
+++ src/components/Container/Container.js
@@ -0,0 +1,11 @@
+import './Container.css'
+const Container = () => {
+    return (
+        <>
+        <h1>Container</h1>
+      
+        </>
+    );
+}
+
+export default Container;
